8a8b45d2ca8fbed6810fcd6bfa134a5cdc7c827a
minhng-178 <anhminh1782002@gmail.com>
2024-05-22 14:01:54 +0700

    fix

--- src/CommingSoon.jsx
+++ src/CommingSoon.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 const CommingSoon = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="mb-4 text-5xl font-bold text-orange-500">Comming Soon</h1>
-            <p className="text-xl text-gray-700">Please wait...</p>
+            <h1 className="mb-4 text-5xl font-bold text-orange-500">Coming Soon</h1>
+            <p className="text-xl text-gray-700">Xin hãy đợi...</p>
 
             <Link to="/">
                 <button className="px-4 py-2 mt-4 font-semibold text-white bg-orange-500 rounded hover:bg-orange-700-700">
-                    Go Back Home
+                    Trở về
                 </button>
             </Link>
         </div>
